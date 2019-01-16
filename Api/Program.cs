fc7e150debbec9fe534aae98947d920b8b8f6b09
Patrick <patrick@ziad.com.br>
2019-01-16 00:31:51 -0200

    Scafold da solução com 3 projetos

--- Api/Program.cs
+++ Api/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace LibrarySystem
+namespace Api
 {
     public class Program
     {
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -2,39 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using LibrarySystem.Models.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace LibrarySystem
+namespace Api
 {
     public class Startup
     {
-        private readonly ILogger _logger;
-        public IConfiguration _configuration { get; }
-        public IHostingEnvironment _environment { get; }
-
-        public Startup(IConfiguration configuration, IHostingEnvironment environment, ILogger<Startup> logger)
+        public Startup(IConfiguration configuration)
         {
-            _configuration = configuration;
-            _environment = environment;
-            _logger = logger;
+            Configuration = configuration;
         }
+
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Configuração de conexão com o PostgreSql
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(_configuration.GetConnectionString("DefaltConnection")));
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
